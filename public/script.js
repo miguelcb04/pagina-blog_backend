6663244769798d7f8b3b630e91c5eb765c467bbe
Miguel Chacón Barranco <miguelcb127@gmail.com>
2024-05-30 13:31:10 +0200

    modificacion del archivo script.js para resolver la paginación

--- public/script.js
+++ public/script.js
@@ -2,7 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
   let postList = document.getElementById("posts-list");
   let links = document.getElementById("links");
 
-  function updatePosts(url) {
+  // Definimos updatePosts en el ámbito global
+  window.updatePosts = function(url) {
     if (url) {
       // Reiniciamos los posts actuales
       postList.innerHTML = "";
@@ -12,14 +13,14 @@ document.addEventListener('DOMContentLoaded', () => {
         .then(res => {
           console.log('Response from API:', res); // Añade esto para depuración
 
-          // Verificamos si res es un array
-          if (!Array.isArray(res)) {
-            console.error('Response is not an array:', res);
-            throw new TypeError("Response is not an array");
+          // Verificamos si res.posts es un array
+          if (!Array.isArray(res.posts)) {
+            console.error('Response posts is not an array:', res.posts);
+            throw new TypeError("Response posts is not an array");
           }
 
           // Obtenemos y recorremos los posts obtenidos
-          for (let post of res) {
+          for (let post of res.posts) {
             // Ingresamos el título y contenido del post
             postList.innerHTML += `<div class="card">
                                       <h3>${post.author}</h3>
@@ -28,15 +29,14 @@ document.addEventListener('DOMContentLoaded', () => {
           }
 
           // Pintamos los enlaces de siguiente o anterior de la paginación
-          // Pintamos los enlaces de siguiente o anterior de la paginación
           links.innerHTML = "";
-          console.log(res.page, res.totalPages)
-          
+          console.log(res.page, res.total_posts);
+
           if (res.page > 1) {
-            links.innerHTML += `<button onclick="updatePosts('http://localhost:3001/api/posts?page=${res.page - 1}')">Atrás</button>`;
+            links.innerHTML += `<button onclick="updatePosts('http://localhost:3000/api/posts?page=${res.page - 1}')">Atrás</button>`;
           }
-          if (res.page < res.totalPages) {
-            links.innerHTML += `<button onclick="updatePosts('http://localhost:3001/api/posts?page=${res.page + 1}')">Siguiente</button>`;
+          if (res.page < res.total_posts) {
+            links.innerHTML += `<button onclick="updatePosts('http://localhost:3000/api/posts?page=${res.page + 1}')">Siguiente</button>`;
           }
         })
         .catch(error => {
